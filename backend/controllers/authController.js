225e7e3aa5ffcfed3791bbfebabcdd41a3ef54e4
Krishna <krishna9842740994@gmail.com>
2026-08-20 21:51:19 +0530

    middleware api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -114,7 +114,30 @@ const loginUser = async (req, res) => {
 };
 
 
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      user,
+    });
+  } catch (error) {
+    console.error("Get user error:", error.message);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+
 
 module.exports = {
-  registerUser ,loginUser
+  registerUser ,loginUser,getMe
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express=require('express');
 
-const {registerUser,loginUser}=require('../controllers/authController');
+const {registerUser,loginUser,getMe}=require('../controllers/authController');
 
-
+const protect=require('../middlewares/authMiddleware');
 const router=express.Router();
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
+
+router.get('/me',protect,getMe);
 
 module.exports=router;
