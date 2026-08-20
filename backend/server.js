f4822b311f3058a9d2247b1b6df2128f4d3da957
Krishna <krishna9842740994@gmail.com>
2026-08-20 20:13:40 +0530

    backend work start

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 app.use(cors());
@@ -9,12 +11,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "SkillSwap API is running 🚀",
+    message: "SkillSwap API is running ",
   });
 });
 
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
