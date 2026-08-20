422aa9b922152575f8b215890b1c0dab68d863f7
Krishna <krishna9842740994@gmail.com>
2026-08-20 20:34:03 +0530

    register api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors");   
+const authRoutes=require("./routes/authRoutes");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/api/auth',authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
