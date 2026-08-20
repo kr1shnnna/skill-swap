7d9924a33d5cbe8e2449e8bc261f73ce58941164
Krishna <krishna9842740994@gmail.com>
2026-08-20 20:17:55 +0530

    user model cooking

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    skillsToTeach: {
+      type: [String],
+      default: [],
+    },
+
+    skillsToLearn: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
