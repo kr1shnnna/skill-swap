995e6f2dbb07fb321a4727869ce5354ec5431865
Krishna <krishna9842740994@gmail.com>
2026-09-09 19:05:28 +0530

    socket io connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const cors = require("cors");   
-const authRoutes=require("./routes/authRoutes");
-const userRoutes=require("./routes/userRoutes");
-const matchRoutes=require("./routes/matchRoutes");
-const swapRoutes=require("./routes/swapRoutes");
-const messageRoutes = require("./routes/messageRoutes");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const matchRoutes = require("./routes/matchRoutes");
+const swapRoutes = require("./routes/swapRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 
 require("dotenv").config();
 
@@ -13,28 +15,42 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(cors());
 app.use(express.json());
 
-
-app.use('/api/auth',authRoutes);
-app.use('/api/users',userRoutes);
-app.use('/api/matches',matchRoutes);
-app.use('/api/swaps',swapRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/swaps", swapRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 app.get("/", (req, res) => {
   res.json({
-    message: "SkillSwap API is running ",
+    message: "SkillSwap API is running",
   });
 });
 
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
